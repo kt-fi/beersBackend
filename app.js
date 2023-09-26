24bff59688486cb8ffbca8fb6f19903a84559761
Christopher Hillman <katie5five.5@gmail.com>
2023-09-26 23:09:48 +0200

    Updated ENV

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require("mongoose");
 const beerRouter = require("./routers/beerRouter");
 const cors = require('cors')
+require('dotenv').config()
+
 
 const app = express();
 
@@ -10,11 +12,13 @@ app.use(express.json());
 app.use("/beer", beerRouter);
 
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb://localhost:27017/beerApp");
+mongoose.connect(`mongodb+srv://katie5five5:${process.env.MONGO_PWD}@cluster0.nuc9sog.mongodb.net/`);
 
 
 
 
 
 
-app.listen(process.env.PORT || 3000, console.log('SERVER STARTED'))
+app.listen(process.env.PORT || 3001, ()=>{
+    console.log('Server Running')
+})
